0ed79a956707fbab291a239f67160c585f3d32ef
Paulo André <pauloandrejanga@gmail.com>
2025-09-27 17:34:28 -0300

    Commit final

--- intro.py
+++ intro.py
@@ -1,4 +1,5 @@
 import pgzrun
+import random
 from pygame import Rect
 
 WIDTH = 800
@@ -16,18 +17,55 @@ start_rect = Rect(WIDTH//2 - 75, 162, 150, 36)
 exit_rect = Rect(WIDTH//2 - 30, 202, 60, 36)
 music_rect = Rect(340, 280, 45, 45)
 effects_rect = Rect(420, 280, 45, 45)
+back_to_menu_rect = Rect(WIDTH//2 - 100, 222, 200, 36)
+next_level_rect = Rect(WIDTH//2 - 125, 192, 250, 36)
+win_menu_rect = Rect(WIDTH//2 - 80, 242, 160, 36)
 
-
-level_map = [
-    "                ",
-    "                ",
-    "    E            ",
-    "    T            ",
-    "     T          ",
-    "   T       E    ",
-    "                ",
-    "  F             ",
-    "TTTTTTTTTTTTTTTT",
+LEVEL_MAPS = [
+    [
+        "                ",
+        "                ",
+        "              F ", 
+        "            TTT ", 
+        " T        T     ",
+        "      E T       ",
+        "    T           ",
+        "  T      E      ",
+        "TTTTTTTTTTTTTTTT",
+    ],
+    [
+        "                ",
+        "                ",
+        "                ",
+        "    T  E     F  ",
+        "T           TTTT",
+        "      T         ",
+        "  TT  E   TTT   ",
+        "                ",
+        "TTTTTTTTTTTTTTTT",
+    ],
+    [
+        "             F  ",
+        "       T     T  ",
+        "    T     T     ",
+        " T       E      ",
+        "       T T      ",
+        "     T     T    ",
+        "   T    E    T  ",
+        "                ",
+        "TTTTTTTTTTTTTTTT",
+    ],
+    [
+        "                ",
+        "  F             ",
+        "  TT            ",
+        "       T T     E",
+        "              TT",
+        "            T    ",
+        "        T       ",
+        "     T   E T    ",
+        "TTTTTTTTTTTTTTTT",
+    ]
 ]
 
 tiles = []
@@ -35,6 +73,9 @@ enemies = []
 flag = None
 
 def setup_level():
+    global flag
+    level_map = random.choice(LEVEL_MAPS)
+    
     for row_index, row in enumerate(level_map):
         for col_index, cell in enumerate(row):
             if cell == 'T':
@@ -46,7 +87,7 @@ def setup_level():
                 x = col_index * TILE_WIDTH
                 y = row_index * TILE_HEIGHT
                 enemy = Actor('enemy_right', topleft=(x, y))
-                enemy.direction = 1  # 1 para direita, -1 para esquerda
+                enemy.direction = 1
                 enemy.speed = 0.5
                 enemy.start_x = x - 120
                 enemy.end_x = x + 120
@@ -54,44 +95,32 @@ def setup_level():
             if cell == 'F':
                 x = col_index * TILE_WIDTH
                 y = row_index * TILE_HEIGHT
-                global flag
                 flag = Actor('exit', topleft=(x, y))
 
 class Player:
     def __init__(self):
         self.player = Actor('frog')
         self.player.x = 50
-
         self.player.y = 200
         self.vy = 0
         self.on_ground = False
-        self.speed = 4 
+        self.speed = 4
         self.player.direction = 'right'
+        self.player.lifecount = 3
 
-    def set_frog_normal_right(self):
-        self.player.image = 'frog'
-
-    def set_frog_normal_left(self):
-        self.player.image = 'frog_left'
+    def set_frog_normal_right(self): self.player.image = 'frog'
+    def set_frog_normal_left(self): self.player.image = 'frog_left'
 
     def update(self, tiles_list, enemies_list):
         prev_x = self.player.x
-        prev_y = self.player.y
-
         if keyboard.D:
             self.player.x += self.speed
-            if keyboard.W:
-                self.player.image = 'frog_rest_right'
-            else:
-                self.player.image = 'frog_jump_right'
+            self.player.image = 'frog_jump_right'
             clock.schedule_unique(self.set_frog_normal_right, 0.25)
             self.player.direction = 'right'
         if keyboard.A:
             self.player.x -= self.speed
-            if keyboard.W:
-                self.player.image = 'frog_rest_left'
-            else:
-                self.player.image = 'frog_jump_left'
+            self.player.image = 'frog_jump_left'
             clock.schedule_unique(self.set_frog_normal_left, 0.25)
             self.player.direction = 'left'
 
@@ -99,13 +128,11 @@ class Player:
             if self.player.colliderect(tile):
                 self.player.x = prev_x
                 break
-
         self.vy += 1
-        if self.vy > 10:
-            self.vy = 10
+        if self.vy > 10: self.vy = 10
         self.player.y += self.vy
 
-        self.on_ground = False 
+        self.on_ground = False
         for tile in tiles_list:
             if self.player.colliderect(tile):
                 if self.vy > 0:
@@ -116,28 +143,37 @@ class Player:
                     self.player.top = tile.bottom
                     self.vy = 0
                 break
-    
+        
         for enemy in enemies_list:
             if self.player.colliderect(enemy):
-                self.player.x = 50
-                self.player.y = 200
-                self.vy = 0
+                self.player.x = 50; self.player.y = 200; self.vy = 0
+                if effects_state == 'on': sounds.jingles_steel07.play()
+                self.player.lifecount -= 1
                 break
         
-        if self.player.left < 0:
-            self.player.left = 0
-        if self.player.right > WIDTH:
-            self.player.right = WIDTH
+        if self.player.left < 0: self.player.left = 0
+        if self.player.right > WIDTH: self.player.right = WIDTH
 
     def jump(self):
         if self.on_ground:
-            if effects_state == 'on':
-                sounds.jump.play()
+            if effects_state == 'on': sounds.jump.play()
             self.vy = -15
             self.on_ground = False
 
+def start_new_game():
+    global game_state, player, flag
+    player = Player()
+    tiles.clear()
+    enemies.clear()
+    flag = None
+    setup_level()
+    game_state = 'game'
+    if music_state == 'on':
+        music.play('music')
+        music.set_volume(0.3)
+
 player = Player()
-setup_level()
+
 def update():
     if game_state == 'game':
         player.update(tiles, enemies)
@@ -145,52 +181,70 @@ def update():
             enemy.x += enemy.speed * enemy.direction
             if enemy.right > enemy.end_x or enemy.left < enemy.start_x:
                 enemy.direction *= -1
-                if enemy.direction == 1:
-                    enemy.image = 'enemy_right'
-                else:
-                    enemy.image = 'enemy_left'
+                enemy.image = 'enemy_right' if enemy.direction == 1 else 'enemy_left'
 
 def draw():
     screen.clear()
+    screen.blit('bg', (0, 0))
+    
+    if game_state == 'game' or game_state == 'win' or game_state == 'game_over':
+        draw_game_elements()
+
     if game_state == 'intro':
-        screen.blit('bg', (0, 0))
         draw_menu()
-    elif game_state == 'game':
-        screen.blit('bg', (0, 0))
-        draw_game()
+    elif game_state == 'win':
+        draw_win_screen()
+    elif game_state == 'game_over':
+        draw_game_over()
+
+def draw_game_elements():
+    player.player.draw()
+    for tile in tiles: tile.draw()
+    for enemy in enemies: enemy.draw()
+    if flag:
+        flag.draw()
+    screen.blit("heart", (10,10))
+    screen.draw.text(str(player.player.lifecount), center=(42, 45), fontsize=50, color="white")
+    check_game_status()
+
+def check_game_status():
+    global game_state
+    if flag and player.player.colliderect(flag):
+        music.stop()
+        if effects_state == 'on' and game_state != 'win':
+            sounds.jingles_sax10.play()
+        game_state = 'win'
+    if player.player.lifecount <= 0 and game_state == 'game':
+        music.stop()
+        game_state = 'game_over'
+
 
 music_clicked = False
 def draw_menu():
     screen.blit('logo', (250, 20))
     screen.draw.text("Iniciar jogo", center=(WIDTH//2, 180), fontsize=30, color="white")
     screen.draw.text("Sair", center=(WIDTH//2, 220), fontsize=30, color="white")
-
     global music_clicked
     if music_state == 'on':
         screen.blit('btn_music_on', (340, 280))
         if not music_clicked:
-            music.play('music')
-            music_clicked = True
+            music.play('music'); music.set_volume(0.3); music_clicked = True
     elif music_state == 'off':
         screen.blit('btn_music_off', (340, 280))
         if music_clicked:
-            music.stop()
-            music_clicked = False
+            music.stop(); music_clicked = False
+    screen.blit('btn_effects_on' if effects_state == 'on' else 'btn_effects_off', (420, 280))
 
-    if effects_state == 'on':
-        screen.blit('btn_effects_on', (420, 280))
-    else:
-        screen.blit('btn_effects_off', (420, 280))
+def draw_win_screen():
+    screen.draw.filled_rect(Rect(0, 0, WIDTH, HEIGHT), (0, 0, 0, 150))
+    screen.draw.text("VOCÊ VENCEU!", center=(WIDTH//2, 120), fontsize=60, color="yellow")
+    screen.draw.text("Jogar novamente", center=(WIDTH//2, 210), fontsize=30, color="white")
+    screen.draw.text("Menu principal", center=(WIDTH//2, 260), fontsize=30, color="white")
 
-def draw_game():
-    player.player.draw()
-    for tile in tiles:
-        tile.draw()
-    for enemy in enemies:
-        enemy.draw()
-    flag.draw()
-    if player.player.colliderect(flag):
-        print('You win!')
+def draw_game_over():
+    screen.draw.filled_rect(Rect(0, 0, WIDTH, HEIGHT), (0, 0, 0, 150))
+    screen.draw.text("VOCÊ PERDEU", center=(WIDTH//2, 150), fontsize=60, color="red")
+    screen.draw.text("Voltar ao Menu", center=(WIDTH//2, 240), fontsize=30, color="white")
 
 def on_key_down(key):
     if game_state == 'game' and key == keys.W:
@@ -198,49 +252,42 @@ def on_key_down(key):
 
 def on_mouse_down(pos):
     global game_state, music_state, effects_state
-    if start_rect.collidepoint(pos):
-        game_state = 'game'
-    elif exit_rect.collidepoint(pos):
-        quit()
-    elif music_rect.collidepoint(pos):
-        if music_state == 'on':
-            music_state = 'off'
-        else:
-            music_state = 'on'
-    elif effects_rect.collidepoint(pos):
-        if effects_state == 'on':
-            effects_state = 'off'
-        else:
-            effects_state = 'on'
+    
+    if game_state == 'intro':
+        if start_rect.collidepoint(pos):
+            start_new_game()
+        elif exit_rect.collidepoint(pos):
+            quit()
+        elif music_rect.collidepoint(pos):
+            music_state = 'off' if music_state == 'on' else 'on'
+        elif effects_rect.collidepoint(pos):
+            effects_state = 'off' if effects_state == 'on' else 'on'
+            
+    elif game_state == 'game_over':
+        if back_to_menu_rect.collidepoint(pos):
+            game_state = 'intro'
+
+    elif game_state == 'win':
+        if next_level_rect.collidepoint(pos):
+            start_new_game()
+        elif win_menu_rect.collidepoint(pos):
+            game_state = 'intro'
 
 def animate_frog():
-    if game_state != 'game':
-        return
-    
+    if game_state != 'game': return
     if not keyboard.A and not keyboard.D and player.on_ground:
         if player.player.direction == 'right':
-            if player.player.image == 'frog':
-                player.player.image = 'frog_closed_eyes_right'
-            else:
-                player.player.image = 'frog'
-        else: 
-            if player.player.image == 'frog_left':
-                player.player.image = 'frog_closed_eyes_left'
-            else:
-                player.player.image = 'frog_left'
+            player.player.image = 'frog_closed_eyes_right' if player.player.image == 'frog' else 'frog'
+        else:
+            player.player.image = 'frog_closed_eyes_left' if player.player.image == 'frog_left' else 'frog_left'
 
 def animate_enemies():
+    if game_state != 'game': return
     for enemy in enemies:
         if enemy.direction == 1:
-            if enemy.image == 'enemy_right':
-                enemy.image = 'enemy_right_closed'
-            else:
-                enemy.image = 'enemy_right'
+            enemy.image = 'enemy_right_closed' if enemy.image == 'enemy_right' else 'enemy_right'
         else:
-            if enemy.image == 'enemy_left':
-                enemy.image = 'enemy_left_closed'
-            else:
-                enemy.image = 'enemy_left'
+            enemy.image = 'enemy_left_closed' if enemy.image == 'enemy_left' else 'enemy_left'
 
 clock.schedule_interval(animate_frog, 0.3)
 clock.schedule_interval(animate_enemies, 0.2)
